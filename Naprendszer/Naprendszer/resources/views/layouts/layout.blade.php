be614e12f2931a06df0d143e239c41cd5527791a
Szilaay <86633348+Szilaay@users.noreply.github.com>
2025-02-17 08:14:31 +0100

    added naprendszer

---
+++ Naprendszer/Naprendszer/resources/views/layouts/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naprendszer</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mystyle.css') }}">
+</head>
+<body class="bg-light">
+    <header class="d-flex justify-content-center align-items-center" style="height: 300px;">
+        <h1 class="text-center">Naprendszer</h1>
+    </header>
+    <div class="container-fliud bg-dark">
+        <nav class="container navbar navbar-expand bg-dark navbar-dark">    
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Főoldal</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Bolygók</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Holdak</a>
+            </li>  
+          </ul>
+        </nav>
+    </div>
+    <main class="container pb-2">
+        @yield('content')
+    </main>
+    <footer class="container">
+        <hr>
+        <p class="text-center">Szücs Szilárd - 13.B</p>
+    </footer>
+</body>
+</html>
---
+++ Naprendszer/Naprendszer/resources/views/layouts/planets.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="flex-wrap pt-3 d-flex justify-content-center">
+    <a href="" class="mx-1 btn btn-dark">Merkúr</a>
+    <a href="" class="mx-1 btn btn-dark">Vénusz</a>
+    <a href="" class="mx-1 btn btn-dark">Föld</a>
+    <a href="" class="mx-1 btn btn-dark">Mars</a>
+    <a href="" class="mx-1 btn btn-dark">Jupiter</a>
+    <a href="" class="mx-1 btn btn-dark">Merkúr</a>
+    <a href="" class="mx-1 btn btn-dark">Merkúr</a>
+    <a href="" class="mx-1 btn btn-dark">Merkúr</a>
+</div>
+@endsection
